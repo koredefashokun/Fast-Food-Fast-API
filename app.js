d52d4009359a438eb8eae9f0da7173eb6e9f009f
Oluwakorede Fashokun <koredefashokun@gmail.com>
2018-09-06 14:32:06 +0100

    Adding coveralls

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-require('dotenv').load();
 import express from 'express';
 import bodyParser from 'body-parser';
 
 import routes from './routes';
-if (process.env.NODE_ENV = 'test') {
-  const port = 4500;
-} else {
-  const port = process.env.PORT || 4500;
-}
+const port = 4500;
 
 const app = express();
 app.use(bodyParser.json());
